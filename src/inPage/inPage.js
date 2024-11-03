a452869b250aadbe0f876b5a8e9c75f9c168bc81
momoirodouhu <momoirodouhu@gmail.com>
2024-11-03 18:58:31 +0900

    remove result element on recheck from inpage script

--- src/inPage/inPage.js
+++ src/inPage/inPage.js
@@ -3,6 +3,7 @@ async function checkInpage() {
     try {
         const { default: getATProtocolHundle } = await import(browser.runtime.getURL("/commonjs/getATProtocolHundle.js"))
         const hundle = await getATProtocolHundle(document.URL)
+        document.getElementById("Bridgy-Fed-Finder")?.remove()
         var inpageNotice = document.createElement("div")
         inpageNotice.setAttribute("id", "Bridgy-Fed-Finder")
         var resultElem = document.createElement("p")
@@ -11,6 +12,7 @@ async function checkInpage() {
         inpageNotice.appendChild(resultElem)
         document.body.appendChild(inpageNotice)
     } catch (error) {
+        document.getElementById("Bridgy-Fed-Finder")?.remove()
         console.debug("Brdgy Fed Finder: " + (browser.i18n.getMessage(error.message) || error.message))
     }
 }
