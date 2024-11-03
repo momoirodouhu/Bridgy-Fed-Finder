57be4337c2fc7849f3f3bd393e14edffd7a51327
momoirodouhu <momoirodouhu@gmail.com>
2024-11-03 21:56:19 +0900

    inPage base UI without styles

--- src/inPage/inPage.js
+++ src/inPage/inPage.js
@@ -3,18 +3,32 @@ async function checkInpage() {
     try {
         const { default: getATProtocolHundle } = await import(browser.runtime.getURL("/commonjs/getATProtocolHundle.js"))
         const hundle = await getATProtocolHundle(document.URL)
-        document.getElementById("Bridgy-Fed-Finder")?.remove()
-        var inpageNotice = document.createElement("div")
-        inpageNotice.setAttribute("id", "Bridgy-Fed-Finder")
-        var resultElem = document.createElement("p")
-        resultElem.innerText = hundle
-        resultElem.style.setProperty('position', 'absolute')
-        inpageNotice.appendChild(resultElem)
-        document.body.appendChild(inpageNotice)
+        await removeNoticeUI()
+        await addNoticeUI(hundle)
     } catch (error) {
-        document.getElementById("Bridgy-Fed-Finder")?.remove()
+        await removeNoticeUI()
         console.debug("Brdgy Fed Finder: " + (browser.i18n.getMessage(error.message) || error.message))
     }
+}
+
+async function addNoticeUI(hundle) {
+    htmlText = await (await fetch(browser.runtime.getURL("/inPage/inPageFoundUI.html"))).text()
+    const inPageFoundUI = new DOMParser().parseFromString(htmlText, "text/html").body.firstElementChild
+    inPageFoundUI.setAttribute("id", "Bridgy-Fed-Finder")
+    inPageFoundUI.querySelector("#result").innerText = hundle
+    inPageFoundUI.querySelector("#open-bsky").addEventListener('click', () => {
+        window.open("https://bsky.app/profile/" + hundle, '_blank').focus()
+    });
+    inPageFoundUI.querySelector("#open-bsky").disabled = false;
+    const {default: translate} = await import(browser.runtime.getURL("/commonjs/l10n.js"))
+    for (elem of inPageFoundUI.querySelectorAll("[data-l10n]")) {
+        elem.innerText = translate(elem.dataset.l10n)
+    }
+    document.body.appendChild(inPageFoundUI)
+}
+
+async function removeNoticeUI() {
+    document.getElementById("Bridgy-Fed-Finder")?.remove()
 }
 
 //navigate event doesn't work in some browser
